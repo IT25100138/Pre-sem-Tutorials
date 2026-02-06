2888cd726ed0f5cb9a16bcf59a15a54b56168192
IT25100138 <it25100138@my.sliit.lk>
2026-02-06 10:04:53 +0530

    Question 05

---
+++ IT25100138Q05.java
@@ -0,0 +1,35 @@
+public class IT25100138Q05 {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int square(int a) {
+        return a * a;
+    }
+ 
+    public static void main(String[] args) {
+        IT25100138Q05 calc = new IT25100138Q05();
+
+        int part1 = calc.multiply(3, 4);
+        int part2 = calc.multiply(5, 7);
+        int sum1 = calc.add(part1, part2);
+        int result1 = calc.square(sum1);
+
+        System.out.println("Result of (3 * 4 + 5 * 7)^2 = " + result1);
+
+        int sum2 = calc.add(4, 7);
+        int square1 = calc.square(sum2);
+
+        int sum3 = calc.add(8, 3);
+        int square2 = calc.square(sum3);
+
+        int result2 = calc.add(square1, square2);
+
+        System.out.println("Result of (4 + 7)^2 + (8 + 3)^2 = " + result2);
+    }
+}
